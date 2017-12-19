f389996986db537250ec86b7ef0988a90e231d51
PowellBrand <powellbrandon@outlook.com>
2017-12-19 11:57:43 -0700

    fixed some stuff

--- session/src/App.js
+++ session/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  click() {
+    axios.put('/api/session_name')
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.click}>click me</button>
       </div>
     );
   }
